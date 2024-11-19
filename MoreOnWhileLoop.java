140d58945694edeaef0a7f2c6495e6b963dc6332
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2024-11-19 15:50:22 +0530

    while loop

---
+++ MoreOnWhileLoop.java
@@ -0,0 +1,11 @@
+public class MoreOnWhileLoop{
+    public static void main(String[] args){
+        int [] nums = {10, 20, 30 , 40, 50};
+        int ind = 0;
+        while (ind < 5){
+            System.out.println(nums[ind]);
+            ind++;
+        }    
+    }
+}
+/* Practice more while loops */
