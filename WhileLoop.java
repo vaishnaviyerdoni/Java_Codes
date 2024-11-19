16c28f4a8dbcf81e51ce3648ff8529d6c4878dbb
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2024-11-19 15:49:29 +0530

    while loop

---
+++ WhileLoop.java
@@ -0,0 +1,9 @@
+public class WhileLoop {
+    public static void main(String[] args){
+        int x = 0;
+        while (x < 4){
+            System.out.println(x);
+            x++;
+        }
+    }    
+}
---
+++ WhileLoopOne.java
@@ -0,0 +1,12 @@
+public class WhileLoopOne{
+    public static void main(String[] args){
+        int i  = 10;
+        while (i+1 <= 20){
+            System.out.println(i);
+            i++;
+        }
+    
+    }
+}
+/* Work on the logic of this one and get the expected result */
+/* And Practice more loops */
