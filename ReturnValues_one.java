82733d6d901af13e202ae3bee53b540827223108
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2024-11-21 23:38:15 +0530

    user defined functions and return values

---
+++ ReturnValues_one.java
@@ -0,0 +1,8 @@
+public class ReturnValues_one {
+    public static void main(String[] args){
+        System.out.println(myfun(20));
+    }
+    static int myfun(int no){
+        return 5 + no;
+    }
+}
---
+++ ReturnValues_two.java
@@ -0,0 +1,9 @@
+public class ReturnValues_two {
+    public static void main(String[] args){
+        int ans = Addition(3,90);
+        System.out.println(ans);
+    }
+    public static int Addition(int no1, int no2){
+        return no1+no2;
+    }
+}
