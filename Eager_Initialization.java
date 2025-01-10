137db4c818ab4d2c634aedf7d3f85f42338851d9
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-01-10 23:38:04 +0530

    Eager Initialization in java

---
+++ Eager_Initialization.java
@@ -0,0 +1,14 @@
+public class Eager_Initialization {
+    private static Eager_Initialization ConObj = new Eager_Initialization();
+    private Eager_Initialization(){
+    }
+
+    public static Eager_Initialization getInstance1(){
+        return ConObj;
+    }
+    
+    public static void main(String[] args) {
+        Eager_Initialization ConnObj = Eager_Initialization.getInstance1();
+        System.out.println(ConnObj + " Established");
+    }
+}
