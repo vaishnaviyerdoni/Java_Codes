6de73fa7be9382107629f638988eee0d3f978d91
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-01-24 00:03:05 +0530

    Practicing

--- CarsComparator.java
+++ CarsComparator.java
@@ -9,13 +9,13 @@ class Cars{
     }
 }
 public class CarsComparator{
-    public static void main(String a[]){
+    public static void main(String[] a){
         Cars[] CarArray = new Cars[3];
         CarArray[0] = new Cars("SUV", "Petrol");
         CarArray[1] = new Cars("Sedan", "Diesel");
         CarArray[2] = new Cars("Hatchback", "Cng");
 
-        Arrays.sort(CarArray, (Cars obj1, Cars obj2) -> obj1.CarName.compareTo(obj2.CarName))
+        Arrays.sort(CarArray, (Cars obj1, Cars obj2) -> obj1.CarName.compareTo(obj2.CarName));
 
         for(Cars car : CarArray){
             System.out.println(car.CarName + "-" + car.CarFuel);
