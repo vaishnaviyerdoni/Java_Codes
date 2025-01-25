712ca6286bda04558c1b1f3f5d6cfdb835462628
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-01-25 23:20:03 +0530

    Linked Hash Map

---
+++ MyLinkedHashMap.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class MyLinkedHashMap {
+    public static void main(String[] a){
+        Map<Integer, String> MyMap = new LinkedHashMap<>();
+        MyMap.put(1, "Harry");
+        MyMap.put(2, "Ron");
+        MyMap.put(3, "Hermione");
+        
+        MyMap.get(2);
+        MyMap.forEach((Integer key, String val) -> System.out.println(key + " : " + val));
+    }
+    
+}
