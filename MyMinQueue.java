26d1adff5cbbbe151445a9a97286e616f2f6077f
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-02-01 23:59:03 +0530

    Min Priority Queue

---
+++ MyMinQueue.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class MyMinQueue{
+    public static void main(String[] args){
+        PriorityQueue<Integer> minPQ = new PriorityQueue<>();
+
+        minPQ.add(50);
+        minPQ.add(40);
+        minPQ.add(30);
+        minPQ.add(20);
+        minPQ.add(10);
+
+        // This line prints elements from the PQ 
+        minPQ.forEach((Integer val) -> System.out.println(val));
+
+        // Removes the top element from the priority queue
+
+        while(!minPQ.isEmpty()){
+            int val = minPQ.poll();
+            System.out.println("Top Value Removed: " + val);
+        }
+    }
+}
