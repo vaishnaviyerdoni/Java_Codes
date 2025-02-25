ad52ad2305639465d78fb7453cd9c174d62377ab
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-02-25 13:46:55 +0530

    Updating

--- ProfessorGame.java
+++ ProfessorGame.java
@@ -2,31 +2,36 @@ import java.util.*;
 public class ProfessorGame{
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-
         int level = get_level(scan);
         int grade = 0;
-        
-        for(int i = 0; i <= 10; i++){
+        for(int i = 0; i < 10; i++){
             int m = get_int(level);
             int n = get_int(level);
             int ans = m + n;
-
-
-            System.out.print(m + " + " + n +  " = " );
-            for( int j = 0; j <= 3; j++){
-                int Ans = scan.nextInt();
-                if (Ans == ans){
-                    grade++;
-                    break;
-                }else{
-                    System.out.println("EEE");
+            
+            try{
+                System.out.print(m +" + "+ n +" = ");
+                boolean isCorrect = false;
+                for(int j = 0; j < 3; j++){
+                    int userAns = scan.nextInt();
+                    if(userAns == ans){
+                        grade++;
+                        isCorrect = true;
+                        break;
+                    }else{
+                        System.out.println("EEE");
+                    }
                 }
-            scan.close();
-            System.out.println("Final Score: " + grade);
-        } 
+                if (!isCorrect){
+                    System.out.println("Correct Ans: " + ans);
+                }
+            }catch(InputMismatchException e){
+                continue;
+            }
+        }
+        scan.close();
+        System.out.println("Final Grade: "+ grade);
     }
-}
-
     static int get_level(Scanner scan){
         while (true){
             try{
