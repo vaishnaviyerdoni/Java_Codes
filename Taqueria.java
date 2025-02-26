82e1d94226bbd2c8c3a3e8eb1a4744684b5c7c8d
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-02-26 22:43:20 +0530

    LinkedHashMap

---
+++ Taqueria.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Taqueria {
+    public static void main(String[] args){
+        Map<String, Double> Menu = new LinkedHashMap<>();
+        Scanner scan = new Scanner(System.in);
+
+        Menu.put("Baja Taco", 4.25);
+        Menu.put("Burrito", 7.50);
+        Menu.put("Bowl", 8.50);
+        Menu.put("Nachos", 11.00);
+        Menu.put("Quesadilla", 8.50);
+        Menu.put("Super Burrito", 8.50);
+        Menu.put("Super Quesadilla", 9.5);
+        Menu.put("Taco", 3.00);
+        Menu.put("Tortilla Salad", 8.00);
+
+        int total_Bill = 0;
+        System.out.println("Welcome to Taqueria!");
+        System.out.println("What would you like to have?");
+
+        while(scan.hasNextLine()){
+            try{
+                String Order = scan.nextLine();
+                if (Order.equalsIgnoreCase("Done")){
+                    break;
+                }
+
+                if (Menu.containsKey(Order)){
+                    total_Bill += Menu.get(Order);
+                }else{
+                    System.out.println("No such item found in menu.");
+                }
+                
+            }catch(NoSuchElementException e){
+                System.out.println("Error Occured");
+                scan.next();
+            }catch(Exception e){
+                System.out.println("Error");
+            }
+        }
+        scan.close();
+        System.out.println("The total bill is: " + total_Bill + "$");
+    }
+}
