48118012ed2f36493e1d42905c2d9a3ed4173f6f
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-02-27 22:41:34 +0530

    Modified and removed errors

--- GroceryList.java
+++ GroceryList.java
@@ -2,21 +2,18 @@ import java.util.*;
 
 public class GroceryList{
     public static void main(String[] args){
-        Map<Integer, String> grocerylist = new HashMap<>();
+        Map<String, Integer> grocerylist = new LinkedHashMap<>();
 
         Scanner scan = new Scanner(System.in);
 
-        Integer num = 1;
+        System.out.println("Enter the items you'd like: ");
+
         while(scan.hasNextLine()){
             String item = scan.nextLine().trim();
 
-            if (!item.isEmpty()){
-                grocerylist.put(num, item);
-                num++;
-            }else{
-                break;
-            }
-        }
+            grocerylist.put(item, grocerylist.getOrDefault(item,0) + 1);
+
+    }
         scan.close();
         System.out.println(grocerylist);
     }
