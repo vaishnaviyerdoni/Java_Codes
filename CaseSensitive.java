f3c345f837d14deac231670948fe5816baa2ef98
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-03-02 23:31:16 +0530

    To make your input string title cased.

--- CaseSensitive.java
+++ CaseSensitive.java
@@ -1,18 +1,35 @@
 import java.util.*;
-public class CaseSensitive{
-    public static String toTitleCase(String input){
-        
-        Scanner scan = new Scanner(System.in);
 
-        String[] word = input.split(" ");
-        StringBuilder titleCase = new StringBuilder();
-        for (String chars : word){
-            titleCase.append(Character.toUpperCase(chars.charAt(0)))
-            .append(chars.substring(1).toLowerCase())
-            .append(" ");
-        }
-        
+public class CaseSensitive{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the Str: ");
+        String str = scan.nextLine().trim();
+        String MyString = toTitleCase(str);
+        System.out.println(MyString);
         scan.close();
-        return titleCase.toString().trim();
+
+    }
+
+    public static String toTitleCase(String input){
+        while(true){
+            try{
+                String[] word = input.split(" ");
+
+                StringBuilder titleCase = new StringBuilder();
+                for(String chars : word){
+                    titleCase.append(Character.toUpperCase(chars.charAt(0)))
+                    .append(chars.substring(1).toLowerCase())
+                    .append(" ");
+                }
+                return titleCase.toString().trim();
+            }
+            catch(StringIndexOutOfBoundsException e){
+                System.out.println("Oops! Enter your string again!");
+            }
+            catch(Exception e){
+                System.out.println("Unexpected Error Occured!");
+            }
+        }   
     }
 }
