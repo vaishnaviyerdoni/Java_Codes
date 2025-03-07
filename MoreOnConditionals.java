773f14904a3b4034ebceeacfd01708d0dfe9dca4
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-03-07 22:47:58 +0530

    Added more conditions

--- MoreOnConditionals.java
+++ MoreOnConditionals.java
@@ -1,16 +1,24 @@
+import java.util.*;
 public class MoreOnConditionals{
     public static void main(String[] args){
-        int time = 4;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the time: ");
+        int time = scan.nextInt();
         String ans = MyFunc(time);
         System.out.println(ans);
+        scan.close();
     }
 
     public static String MyFunc(int time){
-        if (time < 12){
+        if (time == 24 || time == 00){
+            return "Its Night!";
+        }if (time > 4 && time <= 6){
+            return "Its Dawn!";
+        }else if(time > 6 && time < 11){
             return "Its Morning!";
-        }else if(time < 16){
-            return "Its preEvening!";
-        }else if(time < 20){
+        }else if(time > 12 && time <= 16){
+            return "Its AfterNoon!";
+        }else if(time > 17 && time <= 20){
             return "Its Evening";
         }else{
             return "Its Night";
