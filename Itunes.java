3e1779262ff74b01f65ac282b643a54345fbd4b6
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-03-12 23:36:55 +0530

    Attempting a get request

---
+++ Itunes.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.net.*;
+import org.json.JSONObject;
+
+public class Itunes {
+    public static void main(String[]args){
+        try{
+            if (args.length != 1){
+                System.out.println("Too few Arguments!");
+                System.exit(1);
+            }
+
+            String term = args[0];
+            String urlString = "https://itunes.apple.com/search?entity=song&limit=1&term=" + term;
+
+            @SuppressWarnings("deprecation")
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+
+            conn.setRequestMethod("GET");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+            StringBuilder res = new StringBuilder();
+
+            String line;
+
+            while((line = in.readLine()) != null){
+                res.append(line);
+            }
+
+            in.close();
+
+            JSONObject jsonRes = new JSONObject(res.toString());
+            System.out.println(jsonRes.toString(2));
+        }
+        catch(Exception e){
+            System.out.println("Exception Occured!");
+        }
+    }
+}
