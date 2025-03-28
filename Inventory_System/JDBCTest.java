9f84a50621807a8b123a4676a6c9f7abb80fbd44
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-03-28 22:01:20 +0530

    Connecting to sql server using jdbc driver from inside of a package

---
+++ Inventory_System/JDBCTest.java
@@ -0,0 +1,21 @@
+package Inventory_System;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCTest {
+    public static void main(String args[]){
+        String CS = "jdbc:sqlserver://localhost;databaseName=InventoryDB;encrypt=false;integratedSecurity=true";
+        try{
+            try(Connection connection = DriverManager.getConnection(CS)){
+                if (connection != null){
+                System.out.println("Connection Established Successfully!");
+                }
+            }
+        }catch (SQLException e){
+            System.out.println("Connection Failed!");
+            e.printStackTrace();
+        }
+    }
+}
