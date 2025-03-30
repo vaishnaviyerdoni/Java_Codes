1a58715a8e1918baa7c619082676e71dd5962f6e
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-03-30 23:48:41 +0530

    Creating POJO classes to map database tables

---
+++ Inventory_System/Model_Layer/Inventory.java
@@ -0,0 +1,82 @@
+package Inventory_System.Model_Layer;
+
+public class Inventory {
+    
+    //Fields
+    int item_id;
+    String item_name;
+    String category;
+    double price;
+    int quantity;
+    int Low_Stock_Threshold;
+
+    //default Constructor
+    public Inventory(int item_id, String item_name, String category, double price, int quantity, int Low_Stock_Threshold){
+        this.item_id = item_id;
+        this.item_name = item_name;
+        this.category = category;
+        this.price = price;
+        this.quantity = quantity;
+        this.Low_Stock_Threshold = Low_Stock_Threshold;
+    }
+
+    //Getters
+
+    public int get_item_id(){
+        return item_id;
+    }
+
+    public String get_item_name(){
+        return item_name;
+    }
+
+    public String get_category(){
+        return category;
+    }
+
+    public double get_price(){
+        return price;
+    }
+
+    public int get_quantity(){
+        return quantity;
+    }
+
+    public int get_Low_Stock(){
+        return Low_Stock_Threshold;
+    }
+
+    //Setters
+
+    public void set_item_id(int item_id){
+        this.item_id = item_id;
+    }
+
+    public void set_item_name(String item_name){
+        this.item_name = item_name;
+    }
+
+    public void set_category(String category){
+        this.category = category;
+    }
+
+    public void set_quantity(int quantity){
+        this.quantity = quantity;
+    }
+
+    public void set_LowStock(int Low_Stock_Threshold){
+        this.Low_Stock_Threshold = Low_Stock_Threshold;
+    }
+
+    public String toString(){
+        return "InventoryItem{" +
+                "itemId=" + itemId +
+                ", itemName='" + itemName + '\'' +
+                ", category='" + category + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                ", lowStockThreshold=" + lowStockThreshold +
+                '}';
+    }
+
+}
