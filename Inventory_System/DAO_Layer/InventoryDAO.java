42304ac59075fcdacdf5420bd95101d98f453c16
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-04-17 17:20:50 +0530

    CRUD methods for Inventory tables

--- Inventory_System/DAO_Layer/InventoryDAO.java
+++ Inventory_System/DAO_Layer/InventoryDAO.java
@@ -5,38 +5,39 @@ import java.sql.*;
 import Inventory_System.Model_Layer.Inventory;
 
 public class InventoryDAO {
-    private static final String cs = "jdbc:sqlserver://localhost;databaseName=InventoryDB;encrypt=false;integratedSecurity=true";
+    private Connection conn;
+
+    //Constructor to accept database connection
+    public InventoryDAO(Connection conn){
+        this.conn = conn;
+    }
 
     //CREATE method: Add items to the columns in the inventory table
-    public void addItem(Inventory item){
-        String sqlQuery = "INSERT INTO InventoryTable (itemName, category, price, quantity, LowStockThreshold) VALUES (?,?,?,?,?)";
-        try(Connection conn =  DriverManager.getConnection(cs);
-        PreparedStatement stmt = conn.prepareStatement(sqlQuery)){
+    public void addItem(Inventory item) throws SQLException{
+        String sql = "INSERT INTO InventoryTable (itemName, category, price, quantity, LowStockThreshold) VALUES (?,?,?,?,?)";
+        try(PreparedStatement stmt = conn.prepareStatement(sql)){
                 stmt.setString(1, item.get_itemName());
                 stmt.setString(2, item.get_category());
                 stmt.setDouble(3, item.get_price());
                 stmt.setInt(4, item.get_quantity());
                 stmt.setInt(5, item.get_LowStock());
-                //stmt.executeUpdate();
+            
                 int rows = stmt.executeUpdate();
                 if (rows > 0) {
                     System.out.println("Inserted successfully!");
                 } else {
                     System.out.println("Insert failed.");
                 }
-
-        }catch (SQLException e){
-            e.printStackTrace();
+            }
         }
-        
-    }
 
+        
     //READ method: getting all the items from inventory table
-    public List<Inventory> getAllItems(){
+    public List<Inventory> getAllItems() throws SQLException{
         List<Inventory> items = new ArrayList<>();
-        String sqlquery = "SELECT * FROM InventoryTable";
-        try(Connection conn  = DriverManager.getConnection(cs);
-        PreparedStatement stmt = conn.prepareStatement(sqlquery);
+
+        String sql = "SELECT * FROM InventoryTable";
+        try(PreparedStatement stmt = conn.prepareStatement(sql);
         ResultSet res = stmt.executeQuery()){
             while(res.next()){
                 Inventory item = new Inventory(
@@ -49,55 +50,91 @@ public class InventoryDAO {
                     items.add(item);
                     //System.out.println("Fetched item: " + item.get_itemId());
                 }
-            }
-            catch(SQLException e){
-                e.printStackTrace();
-            }
-        return items;
+            return items;
+        }
     }
-
+    
     //UPDATE method: update the columns from inventory table wrt quantity
-    public void updateItembyQuantity(int itemId, int newQuantity){
+    public void updateItembyQuantity(int itemId, int newQuantity) throws SQLException{
         String sql  = "UPDATE InventoryTable SET quantity = ? WHERE itemId = ?";
-        try(Connection conn = DriverManager.getConnection(cs);
-        PreparedStatement stmt = conn.prepareStatement(sql)){
+        try(PreparedStatement stmt = conn.prepareStatement(sql)){
             stmt.setInt(1, newQuantity);
             stmt.setInt(2, itemId);
             stmt.executeUpdate();
 
-            System.out.println("Inserted Successfully!");
-        }
-        catch(SQLException e){
-            e.printStackTrace();
+            System.out.println("Updated Successfully!");
         }
     }
+
     //DELETE method : delete the record the table for the given id
-    public void deleteItem(int itemId){
-        String sqlQuery = "DELETE FROM InventoryTable WHERE itemId = ?";
-        try(Connection conn = DriverManager.getConnection(cs);
-        PreparedStatement stmt = conn.prepareStatement(sqlQuery)){
+    public void deleteItem(int itemId) throws SQLException{
+        String sql = "DELETE FROM InventoryTable WHERE itemId = ?";
+        try(PreparedStatement stmt = conn.prepareStatement(sql)){
             stmt.setInt(1, itemId);
             stmt.executeUpdate();
             System.out.println("Deleted Successfully");
         }
+    }
+        
+ 
+    public static void main(String[] args) {
+        Connection conn = null;
+        try{
+            conn = DatabaseConnection.getConn();
+        }
         catch(SQLException e){
             e.printStackTrace();
         }
-    }
- 
-    public static void main(String[] args) {
-        
-        InventoryDAO items = new InventoryDAO();
-        //List<Inventory> item = items.getAllItems(); //Reading from the table
+        InventoryDAO items = new InventoryDAO(conn);
 
-        //System.out.println(item);
-        
-        //Inventory item = new Inventory(0,"MOBILE", "ELECTRONICS", 14000.00, 50, 10);
-    
-        //items.addItem(item); //  Creating an entry in table
+        /* 
+        //testing the create method
+        Inventory item = new Inventory(0,"Pen", "Stationary", 20.00, 1000, 100);
+        try{
+            items.addItem(item);
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        */
 
-        //items.updateItembyQuantity(2, 20); // updating an entry in table
-        //items.deleteItem(3); // Deleting and Entry from the table
+        /* 
+        //to test the read method
+        try{  
+            List<Inventory> item = items.getAllItems();
+
+            for (int i = 0; i < item.size(); i++){
+                Inventory myitem = item.get(i);
+                System.out.println("Item Id = " + myitem.get_itemId());
+                System.out.println("item Name = " + myitem.get_itemName());
+                System.out.println("category = " + myitem.get_category());
+                System.out.println("price = " + myitem.get_price());
+                System.out.println("quantity = " + myitem.get_price());
+                System.out.println("Low Stock Threshold = " + myitem.get_LowStock());
+                System.out.println("-----------------");
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        */
+        
+        /* 
+        // test the update method
+        try{
+            items.updateItembyQuantity(8, 500);
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+
+        */
+        
+        //to test the Delete method 
+        try{
+            items.deleteItem(8);
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
     
     }
 }
