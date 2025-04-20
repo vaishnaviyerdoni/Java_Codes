310e41815c7761cca4997437cd00c33c4f378cc2
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-04-20 22:31:26 +0530

    Updated

--- Inventory_System/Business_Layer/InventorySevice.java
+++ Inventory_System/Business_Layer/InventorySevice.java
@@ -1,5 +1,28 @@
 package Inventory_System.Business_Layer;
+import java.util.*;
+import java.sql.*;
+import Inventory_System.DAO_Layer.*;
 
 public class InventorySevice {
-    
+    public static void main(String[] args) {
+        Connection conn = null;
+        try{
+            conn = DatabaseConnection.getConn();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+        InventoryDAO dao = new InventoryDAO(conn);
+        try{
+            List<List<Integer>> idAndLowStock = dao.getLowStockThreshold();
+            for(int i = 0; i < idAndLowStock.size(); i++){
+                int itemId = idAndLowStock.get(i).get(0);
+                int LowStockThreshold = idAndLowStock.get(i).get(1);
+                System.out.println(itemId + " : " + LowStockThreshold);
+            }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
