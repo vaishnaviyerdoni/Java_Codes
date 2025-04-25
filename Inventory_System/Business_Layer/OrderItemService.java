7d27ff7d51ff2e65e53ae58336dcd79a9927e8d4
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-04-25 18:35:00 +0530

    updating

--- Inventory_System/Business_Layer/OrderItemService.java
+++ Inventory_System/Business_Layer/OrderItemService.java
@@ -1,5 +1,18 @@
 package Inventory_System.Business_Layer;
 
+import Inventory_System.DAO_Layer.InventoryDAO;
+import Inventory_System.DAO_Layer.OrderDAO;
+
 public class OrderItemService {
+    private InventoryDAO inventoryDAO;
+    private OrderDAO orderDAO;
+    private UserDAO userDAO;
+
+    public OrderItemService(InventoryDAO inventoryDAO, OrderDAO orderDAO, UserDAO userDAO){
+        this.inventoryDAO = inventoryDAO;
+        this.orderDAO = orderDAO;
+        this.userDAO = userDAO;
+    }
+
     
 }
