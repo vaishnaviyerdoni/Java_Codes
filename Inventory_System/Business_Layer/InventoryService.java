f061455f4bda3761710cb6e86a7ccc86bd356d0d
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-04-30 17:27:39 +0530

    Updating Service class

--- Inventory_System/Business_Layer/InventoryService.java
+++ Inventory_System/Business_Layer/InventoryService.java
@@ -18,46 +18,57 @@ public class InventoryService{
         this.orderItemDAO = orderItemDAO;
     }
 
-    //alert message for Low Quantity
-    public String AlertLowQuantity(int itemId) throws SQLException{
+    //check inventory and return boolean value after comparing quantity and Lowstockthreshold
+    public boolean AlertLowQuantity(int itemId, int userId) throws SQLException{
         try{
-            int quantity = inventoryDAO.getQuantity(itemId);
-            int LowStockThreshold = inventoryDAO.getLowStockThreshold(itemId);
+            String role = userDAO.getRole(userId);
+            if (role.equalsIgnoreCase("Admin") || role.equalsIgnoreCase("staff")){
+                int quantity = inventoryDAO.getQuantity(itemId);
+                int LowStockThreshold = inventoryDAO.getLowStockThreshold(itemId);
 
-            if(quantity <= LowStockThreshold){
-                return "Low Stock!";
-            }
-            else{
-                return "Enough Supply for " + itemId + " available!";
+                if (quantity <= LowStockThreshold){
+                    return true; //return true items are running low
+                }
+                else{
+                    return false; //return false enough items are available in inventory
+                }
             }
         }
         catch(SQLException e){
             e.printStackTrace();
-            return "Error occured while checking the stock!";
+            return true;
         }
     }
 
     //method to compare the quantity and lowstock threshold and update the quantity if necessary
     //rule1: quantity cannot be lesser than or equal to low stock threshold
-    public void CompareQnt(int itemId) throws SQLException{
+    public String CompareQnt(int itemId, int userId, int newQuantity) throws SQLException{
         try{
-            int quantity = inventoryDAO.getQuantity(itemId);
-            int LowStockThreshold = inventoryDAO.getLowStockThreshold(itemId);
-
-            if(quantity <= LowStockThreshold){
-                try{
-                    inventoryDAO.updateItembyQuantity(itemId, 10);
+            String role = userDAO.getRole(userId){
+                if (role.equalsIgnoreCase("admin") || role.equalsIgnoreCase("staff")){
+                    
+                    InventoryService service = new InventoryService(inventoryDAO, userDAO, orderItemDAO);
+                    boolean isLow = service.AlertLowQuantity(itemId, userId);
+                    if (isLow){
+                        if (inventoryDAO.updateItembyQuantity(itemId, newQuantity)){
+                            return "Inventory Updated!";
+                        }
+                        else{
+                            return "Couldn't update inventory!";
+                        }
+                    }
+                    else{
+                        return "Enough items are in inventory for itemId: " : itemId;
+                    }
                 }
-                catch(SQLException e){
-                    e.printStackTrace();
+                else{
+                    return "Customer is not authorized to update inventory!";
                 }
-            }
-            else{
-                System.out.println("Enough items present in the Inventory!");
             }
         }
         catch(SQLException e){
             e.printStackTrace();
+            return "Error Occurred during updating inventory!";
         }
     }
 
@@ -67,16 +78,17 @@ public class InventoryService{
         try{
             String role = userDAO.getRole(userId);
             if (role.equalsIgnoreCase("admin")){
-                try{
-                    inventoryDAO.updatePricebyItemId(itemId, newPrice);
+                if(inventoryDAO.updatePricebyItemId(itemId, newPrice)){
+                    return "Price Updated!";
                 }
-                catch(SQLException e){
-                    e.printStackTrace();
-                }
+            }
+            else{
+                return "Only Admin can update price!";
             }
         }
         catch(SQLException e){
             e.printStackTrace();
+            return "Error Occurred when updating price!";
         }
     }
     
@@ -106,14 +118,14 @@ public class InventoryService{
     }
 
     //to get the items by category
-    public void viewItemsByCategory(String category) throws SQLException{
+    public List<String> viewItemsByCategory(String category) throws SQLException{
         try{
-            List<String> itemsBycategory = new ArrayList<>();
-
-            itemsBycategory = inventoryDAO.getitemByCategory(category);
+            List<String> itemsBycategory = inventoryDAO.getitemByCategory(category);
+            return itemsBycategory;
         }
         catch(SQLException e){
             e.printStackTrace();
+            return null;
         }
     }
 
