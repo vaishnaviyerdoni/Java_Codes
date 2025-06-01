ae753df948c7dbc8e0b7288184c56ff42573c0ea
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-06-01 23:37:59 +0530

    updating frontend

---
+++ Inventory_System/WebContent/User/UsersJS/deleteUserAdmin.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const deleteUserSection = document.getElementById("{deleteUsersByAdminSection");
+    const form = document.getElementById("deleteUserForm");
+
+    form.addEventListener()
+
+})
