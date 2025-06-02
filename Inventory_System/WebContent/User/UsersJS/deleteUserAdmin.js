f497339dee5400c1bccf75ab6d32c7371d3618aa
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-06-02 13:57:43 +0530

    js and html for Inventory system

--- Inventory_System/WebContent/User/UsersJS/deleteUserAdmin.js
+++ Inventory_System/WebContent/User/UsersJS/deleteUserAdmin.js
@@ -2,6 +2,32 @@ document.addEventListener("DOMContentLoaded", () => {
     const deleteUserSection = document.getElementById("{deleteUsersByAdminSection");
     const form = document.getElementById("deleteUserForm");
 
-    form.addEventListener()
+    form.addEventListener("submit", async(e) => {
+        e.preventDefault();
+        const adminID = document.getElementById("AdminID").value.trim();
+        const deleteUserID = document.getElementById("DeleteUserID").value.trim();
 
+        try{
+            const response = await fetch("/user", {
+                method : "DELETE",
+                headers : {"Content-Type" : "application/json"},
+                body : JSON.stringify({
+                    action : "AdminDeletesUser",
+                    adminID,
+                    deleteUserID
+                })
+            });
+
+            const result = await response.text();
+            if(response.ok && result.includes("User was deleted successfully!")){
+                alert("The user is deleted!");
+            }
+            else{
+                document.getElementById("DeleteMessage").innerText = "Failed to delete User, try again later";
+            }
+        }
+        catch(error){
+            document.getElementById("DeleteMessage").innerText = "Server Error, try again later!";
+        }
+    })
 })
