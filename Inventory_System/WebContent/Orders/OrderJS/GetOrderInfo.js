e911c8765dd3e98ef64ea5cf4ba458123dfc0bb0
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-06-06 00:53:49 +0530

    Updating js for the inventory system

---
+++ Inventory_System/WebContent/Orders/OrderJS/GetOrderInfo.js
@@ -0,0 +1,148 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const orderSection = document.getElementById("getAllSection");
+    const formToGetAllOrders = document.getElementById("getAllOrdersForm");
+    formToGetAllOrders.addEventListener("submit", async(e) => {
+        e.preventDefault();
+
+        const userId = document.getElementById("GetAllUserID").value.trim();
+
+        try{
+            const res = await fetch(`/inventory?action=getAllOrders&userId=${encodeURIComponent(userId)}`, {
+                method : "GET"
+            })
+
+            const OrderInfo = await res.json();
+            if(res.ok){
+                orderSection.innerHTML = "<h3>Order Info</h3>";
+                for(let i=0; i<OrderInfo.length; i++){
+                    const data = OrderInfo[i];
+                    const jsonData = `
+                        <p><strong>ItemsID:</strong>${data.itemsId}</p>
+                        <p><strong>OrderID:</strong>${data.orderId}</p>
+                        <p><strong>InventoryID:</strong>${data.inventoryId}</p>
+                        <p><strong>Quantity:</strong>${data.quantity}</p>
+                        <p><strong>Subtotal:</strong>${data.subtotal}</p>
+                        <p><strong>userID:</strong>${data.userId}</p>
+                        <hr>
+                    `
+                    orderSection.innerHTML += jsonData;
+                }
+            }
+            else{
+                document.getElementById("getAllOrdersMessage").innerText = "Failed to fetch data, try again later!";
+            }
+        }
+        catch(error){
+            console.error("Error while fetching data:", error);
+            document.getElementById("getAllOrdersMessage").innerText = "Server Error, try again later!";
+        }
+    })
+
+    const viewSection = document.getElementById("viewbyOrderIDSection");
+    const viewbyOrderIDForm = document.getElementById("viewOrderbyOrderIDForm");
+    viewbyOrderIDForm.addEventListener("submit", async(e) => {
+        e.preventDefault();
+
+        const orderId = document.getElementById("orderID").value.trim();
+        const userId = document.getElementById("viewUserID").value.trim();
+
+        try{
+            const res = await fetch(`/inventory?action=viewByOrderId&orderId=${encodeURIComponent(orderId)}&userId=${encodeURIComponent(userId)}`, {
+                method : "GET"
+            })
+
+            if(res.ok){
+                viewSection.innerHTML = "<h3>Order Information </h3>";
+                for(let i=0; i<OrderInfo.length; i++){
+                    const data = OrderInfo[i];
+                    const jsonDatabyOrderID = `
+                        <p><strong>ItemsID:</strong>${data.itemsId}</p>
+                        <p><strong>OrderID:</strong>${data.orderId}</p>
+                        <p><strong>InventoryID:</strong>${data.inventoryId}</p>
+                        <p><strong>Quantity:</strong>${data.quantity}</p>
+                        <p><strong>Subtotal:</strong>${data.subtotal}</p>
+                        <p><strong>userID:</strong>${data.userId}</p>
+                        <hr>
+                    `
+                    viewSectionSection.innerHTML += jsonDatabyOrderID;
+                }
+            }
+            else{
+                document.getElementById("viewbyIDMessage").innerText = "Failed to fetch data, try again later!";
+            }
+        }
+        catch(error){
+            console.error("Error while fetching data:", error);
+            document.getElementById("viewbyIDMessage").innerText = "Server Error, try again later!";
+        }
+    })
+
+    const ViewByUserIDForm = document.getElementById("viewbyUserIDForm");
+    const viewbyuserIDsection = document.getElementById("getbyUserIDSection");
+
+    ViewByUserIDForm.addEventListener("submit", async(e) => {
+        e.preventDefault();
+        const userId = document.getElementById("viewPriceID").value.trim();
+        try{
+            const res = await fetch(`/inventory?action=viewByUserId&userId=${encodeURIComponent(userId)}`, {
+                method : "GET"
+            })
+
+            const orderbyUserID = await res.json();
+
+            if(res.ok){
+                viewbyuserIDsection.innerHTML = "<h3>Order Information</h3>";
+                for(let i=0; i<orderbyUserID.length; i++){
+                    const data = orderbyUserID[i];
+                    const jsonDatabyUserID = `
+                        <p><strong>ItemsID:</strong>${data.itemsId}</p>
+                        <p><strong>OrderID:</strong>${data.orderId}</p>
+                        <p><strong>InventoryID:</strong>${data.inventoryId}</p>
+                        <p><strong>Quantity:</strong>${data.quantity}</p>
+                        <p><strong>Subtotal:</strong>${data.subtotal}</p>
+                        <p><strong>userID:</strong>${data.userId}</p>
+                        <hr>
+                    `
+                    viewbyuserIDsection.innerHTML += jsonDatabyUserID;
+                }
+            }
+            else{
+                document.getElementById("viewByUserIDMessage").innerText = "Failed to fetch data, try again later!";
+            }
+        }
+        catch(error){
+            console.error("Error while fetching data:", error);
+            document.getElementById("viewByUserIDMessage").innerText = "Server Error, try again later!";
+        }
+    })
+
+    const getPriceSection = document.getElementById("getPriceSection");
+    const getPriceForm = document.getElementById("getPriceForm");
+    
+    getPriceForm.addEventListener("submit", async(e) => {
+        e.preventDefault();
+
+        const userId = document.getElementById("viewPriceID").value.trim();
+
+        try{
+            const res = await fetch(`/inventory?action=getPrice&userId=${encodeURIComponent(userId)}`, {
+            method : "GET"
+            })
+
+            const price = await res.json();
+
+            if(res.ok){
+                getPriceSection.innerHTML = "<h3>Price</h3>";
+                const jsonPrice = `<p><strong>Price:</strong>${price}</p>`;
+                getPriceSection.innerHTML += jsonPrice;
+            }
+            else{
+                document.getElementById("getPriceMessage").innerText = "";
+            }   
+        }
+        catch(error){
+            console.error("Error while fetching data:", error);
+            document.getElementById("getPriceMessage").innerText = "Server Error, try again later!";
+        }
+    })
+})
