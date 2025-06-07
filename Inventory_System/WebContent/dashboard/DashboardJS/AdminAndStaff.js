a28c8eeb69e20390336882fa7bb11477c30930f6
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-06-07 14:27:14 +0530

    Updating dashboard for inventory system

--- Inventory_System/WebContent/dashboard/DashboardJS/AdminAndStaff.js
+++ Inventory_System/WebContent/dashboard/DashboardJS/AdminAndStaff.js
@@ -1,12 +1,12 @@
 // dashboard.js
-const role = localStorage.getItem('role');
+const role = localStorage.getItem("role");
 
 if (!role) {
-  alert('No user role found, redirecting to login.');
-  window.location.href = '../User/login.html';
+  alert("No user role found, redirecting to login!");
+  window.location.href = "../User/login.html";
 }
 
-const deleteUserBtn = document.getElementById('deleteUserBtn');
+const deleteUserBtn = document.getElementById("deleteUserBtn");
 const deleteOrderBtn = document.querySelector(
   "#orderManagementSection button[onclick*='DeleteOrders.html']"
 );
@@ -14,13 +14,13 @@ const deleteInventoryBtn = document.querySelector(
   "#inventoryManagementSection button[onclick*='UpdateNDelete.html']"
 );
 
-if (role === 'staff') {
-  deleteUserBtn.style.display = 'none';
-  deleteOrderBtn.style.display = 'none';
-  deleteInventoryBtn.style.display = 'none';
+if (role === "staff") {
+  if (deleteUserBtn) deleteUserBtn.style.display = "none";
+  if (deleteOrderBtn) deleteOrderBtn.style.display = "none";
+  if (deleteInventoryBtn) deleteInventoryBtn.style.display = "none";
 }
 
-document.getElementById('logoutBtn').addEventListener('click', () => {
-  localStorage.removeItem('role');
-  window.location.href = '../login.html';
+document.getElementById("logoutBtn").addEventListener("click", () => {
+  localStorage.removeItem("role");
+  window.location.href = "../../User/UserHTML/login.html";
 });
