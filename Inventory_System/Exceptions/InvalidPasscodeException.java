ef44104b87a20111aaf01665367d1632a6942387
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-06-26 23:51:35 +0530

    added an exception

---
+++ Inventory_System/Exceptions/InvalidPasscodeException.java
@@ -0,0 +1,8 @@
+package Inventory_System.Exceptions;
+
+public class InvalidPasscodeException extends Exception {
+    
+    public InvalidPasscodeException(String message){
+        super(message);
+    }
+}
