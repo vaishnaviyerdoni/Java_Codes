a582ccc8226302e4ad8b299a6a157442cc45aee2
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-06-29 23:12:07 +0530

    updating customer dashboard

--- Inventory_System/WebContent/dashboard/Customer.js
+++ Inventory_System/WebContent/dashboard/Customer.js
@@ -1,11 +1,16 @@
 //Dashboard.html
-const role = localStorage.getItem("role");
-if (!role || role !== "customer") {
+console.log("🟢 JS is loading");
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("🟢 DOM is loading");
+
+  const role = localStorage.getItem("role");
+  if (!role || role !== "customer") {
     alert("Unauthorized access. Redirecting to login page.");
     window.location.href = "../User/login.html";
-}
+ }
 
-document.getElementById("logoutBtn").addEventListener("click", () => {
+  document.getElementById("logoutBtn").addEventListener("click", () => {
   localStorage.clear();
   window.location.href = "../User/logout.html";
-});
+ });
+})
