6d75e4266ef7d9bf9444d26a15609101d5887473
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-08-22 23:54:39 +0530

    Practicing

--- BreakNContinue.java
+++ BreakNContinue.java
@@ -1,6 +1,6 @@
 public class BreakNContinue{
     public static void main(String[] args){
-        for(int i = 1; i <= 10; i++){
+        for(int i = 1; i <= 20; i++){
             if(i == 4){
                 continue;
             }
