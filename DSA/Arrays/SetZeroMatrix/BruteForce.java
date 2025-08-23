2fdba45fb6e8743f47693d3a4043d02134315371
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-08-23 23:52:00 +0530

    Starting DSA

---
+++ DSA/Arrays/SetZeroMatrix/BruteForce.java
@@ -0,0 +1,7 @@
+package DSA.Arrays.SetZeroMatrix;
+
+public class BruteForce {
+    public static void main(String [] args){
+        System.out.println("Hello DSA!");
+    }
+}
---
+++ DSA/Arrays/SetZeroMatrix/Optimal.java
@@ -0,0 +1,5 @@
+package DSA.Arrays.SetZeroMatrix;
+
+public class Optimal {
+    
+}
