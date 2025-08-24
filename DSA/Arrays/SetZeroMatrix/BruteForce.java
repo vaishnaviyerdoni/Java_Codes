dd53681cbeb26d9d882574f77704c4a04abbb7d6
vaishnaviyerdoni <vaishnaviyerdoni@gmail.com>
2025-08-24 18:59:33 +0530

    BruteForce Soln for Set Zero Matrix

--- DSA/Arrays/SetZeroMatrix/BruteForce.java
+++ DSA/Arrays/SetZeroMatrix/BruteForce.java
@@ -1,7 +1,30 @@
 package DSA.Arrays.SetZeroMatrix;
 
+import java.util.*;
+
 public class BruteForce {
     public static void main(String [] args){
-        System.out.println("Hello DSA!");
+        
+        ArrayList<ArrayList<Integer>> setZeroMatrix = new ArrayList<>(); 
+
+        setZeroMatrix.add(new ArrayList<>(Arrays.asList(1,1,1)));
+        setZeroMatrix.add(new ArrayList<>(Arrays.asList(1,0,1)));
+        setZeroMatrix.add(new ArrayList<>(Arrays.asList(1,1,1)));
+
+        int n = setZeroMatrix.size();
+        int m = setZeroMatrix.get(0).size();
+
+        ArrayList<ArrayList<Integer>> modifiedMatrix = setZero(setZeroMatrix, n, m);
+
+        for(ArrayList<Integer> row : modifiedMatrix) {
+            for(Integer element : row){
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static ArrayList<ArrayList<Integer>> setZero(ArrayList<ArrayList<Integer>> matrix, int n, int m) {
+
     }
 }
